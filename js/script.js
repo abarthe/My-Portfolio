e312a060e575b3e0b30ccea3750d1e53573613dc
Abdallah Barthe <contact@abdallahbarthe.com>
2023-06-09 21:03:46 +0100

    New Update

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*======= typed js =======*/
 const typed = new Typed(".multiple-text", {
-  strings: ["Web Developer", "Digital Maketer", "Traveler"],
+  strings: ["Web Developer", "Digital Maketer", "Globetrotter"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
